1592e5d816e42f920433a7988f86618e258dc969
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-06-26 09:57:11 +0100

    code blocks for the topic, Rust Concurrency Patterns for Parallel Programming

---
+++ section: concepts related to concurrency patterns/error handling in concurrent code/main.rs
@@ -0,0 +1,28 @@
+use std::sync::mpsc::channel;
+use std::thread;
+
+fn main() {
+    let (tx, rx) = channel();
+    let handle = thread::spawn(move || {
+        // Perform som
+        // Send the result over the channel
+        let send_result = tx.send(result);
+        match send_result {
+            Ok(_) => Ok(()),
+            Err(e) => Err(e),
+        }
+    });
+    // Wait for the thread to finish and handle any errors that occur
+    let thread_result = handle.join().unwrap(); // Note: unwrap is safe here because we're propagating any errors through the Result type
+    match thread_result {
+        Ok(_) => {
+            // Receive the result from the channel
+            let result = rx.recv();
+            match result {
+                Ok(val) => println!("Result: {}", val),
+                Err(e) => println!("Error receiving result: {:?}", e),
+            }
+        }
+        Err(e) => println!("Error sending result: {:?}", e),
+    }
+}
---
+++ section: concurrency patterns/arc/main.rs
@@ -0,0 +1,50 @@
+use std::sync::{Arc, Mutex};
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::thread;
+
+fn main() {
+  // File paths to read
+  let file_paths = vec!["file1.txt", "file2.txt", "file3.txt"];
+  let shared_data = Arc::new(Mutex::new(Vec::new()));
+
+  let mut handles = vec![];
+
+  // Spawn threads to read files concurrently
+  for file_path in file_paths {
+      let shared_data = Arc::clone(&shared_data);
+      let handle = thread::spawn(move || {
+          // Open the file
+          let file = File::open(file_path).expect("Failed to open file");
+
+          // Read the contents of the file
+          let lines: Vec<String> = io::BufReader::new(file)
+              .lines()
+              .map(|line| line.expect("Failed to read line"))
+              .collect();
+
+          // Lock the shared data
+          let mut data = shared_data.lock().unwrap();
+
+          // Append the lines to the shared data
+          data.extend(lines);
+      });
+
+      handles.push(handle);
+  }
+
+  // Wait for all threads to finish
+  for handle in handles {
+      handle.join().unwrap();
+  }
+
+  print!("{:#?}", shared_data.lock().unwrap());
+
+  // Explicitly drop the Arc
+  drop(shared_data);
+
+  // Attempting to access the shared data after dropping the Arc would result in a compile-time error
+  // Uncomment the following line to see the error:
+  // let data = shared_data.lock().unwrap();
+}
+
